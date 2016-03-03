e418a9a6a9d2941d01bbf8018285bad0f5f04be6
Lusindiso <lusindiso@projectcodex.co>
2016-03-03 15:58:53 +0200

    lusindiso

--- javascript.js
+++ javascript.js
@@ -1,33 +1,27 @@
 function getRandomIntInclusive(min, max) {
- return Math.floor(Math.random() * (9 - 1 + 1)) + 1;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+var el = document.querySelectorAll(".random");
 
-var el = document.querySelector("#random")
-function upDate(){
-el.innerHTML = getRandomIntInclusive();
-}
-getRandomIntInclusive();
+var upDate = function() {
+  for (i = 0; i < el.length; i++) {
+    el[i].innerHTML = getRandomIntInclusive(1, 3);
+  }
+};
+var match = function() {
+  if (el[0].innerHTML === el[1].innerHTML) {
+    el[0].classList.add("highlight");
+    el[1].classList.add("highlight");
+  }
+  if (el[0].innerHTML === el[2].innerHTML) {
+    el[0].classList.add("highlight");
+    el[2].classList.add("highlight");
+  }
+  if (el[1].innerHTML === el[2].innerHTML) {
+    el[1].classList.add("highlight");
+    el[2].classList.add("highlight");
+  }
+};
+
 upDate();
-
-
-function getRandomIntInclusive(min, max) {
- return Math.floor(Math.random() * (3 - 1 + 1)) + 1;
-}
-
-var el = document.querySelector("#two")
-function upDate(){
-el.innerHTML = getRandomIntInclusive();
-}
-getRandomIntInclusive();
-upDate();
-
-function getRandomIntInclusive(min, max) {
- return Math.floor(Math.random() * (3 - 1 + 1)) + 1;
-}
-
-var el = document.querySelector("#three")
-function upDate(){
-el.innerHTML = getRandomIntInclusive();
-}
-getRandomIntInclusive();
-upDate();
+match();
