48c164714835ba49aa1443b1b76c54cd5759ed62
Lusindiso <lusindiso@projectcodex.co>
2016-03-04 12:31:31 +0200

    lusindiso

--- javascript.js
+++ javascript.js
@@ -9,19 +9,30 @@ var upDate = function() {
   }
 };
 var match = function() {
+  var matchText = document.getElementById("message");
+
   if (el[0].innerHTML === el[1].innerHTML) {
     el[0].classList.add("highlight");
     el[1].classList.add("highlight");
+    matchText.innerHTML = el[0].innerHTML + " " + "are matching";
   }
   if (el[0].innerHTML === el[2].innerHTML) {
     el[0].classList.add("highlight");
     el[2].classList.add("highlight");
+    matchText.innerHTML = el[0].innerHTML + " " + "are matching";
   }
   if (el[1].innerHTML === el[2].innerHTML) {
     el[1].classList.add("highlight");
     el[2].classList.add("highlight");
+    matchText.innerHTML = el[0].innerHTML + " " + "are matching";
   }
+
 };
+
+// var displayMessage = function(text) {
+//   var message = document.getElementById("message");
+//   message.innerHTML = getRandomIntInclusive(1, 3);
+
 
 upDate();
 match();
